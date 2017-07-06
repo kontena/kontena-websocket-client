2c9f38c2028adfffced01ddd0e040af72a6f783a
Tero Marttila <tero.marttila@kontena.io>
2017-07-06 19:39:46 +0300

    common websocket-client spec: SSL unverifyable cert

--- spec/kontena/websocket/spec.rb
+++ spec/kontena/websocket/spec.rb
@@ -1,6 +1,9 @@
 require 'kontena-websocket-client'
 
 describe Kontena::Websocket::Client do
+  let(:logger) {
+    Logger.new(STDERR)
+  }
   context "For a server that is ECONNREFUSED" do
     subject { described_class.new('ws://127.0.0.1:1337') }
 
@@ -18,16 +21,16 @@ describe Kontena::Websocket::Client do
   end
 
   context "For a local server" do
-    let(:server) do
+    let(:tcp_server) do
       TCPServer.new('127.0.0.1', 0)
     end
     let(:port) do
-      af, port, hostname, ip = server.addr
+      af, port, hostname, ip = tcp_server.addr
       port
     end
 
     before do
-      server
+      tcp_server
       server_thread
     end
 
@@ -41,7 +44,7 @@ describe Kontena::Websocket::Client do
       let(:server_thread) do
         Thread.new do
           loop do
-            client = server.accept
+            client = tcp_server.accept
             client.close
           end
         end
@@ -58,7 +61,7 @@ describe Kontena::Websocket::Client do
       let(:server_thread) do
         Thread.new do
           loop do
-            client = server.accept
+            client = tcp_server.accept
             client.readpartial(1024)
             client.write([
               "HTTP/1.1 404 Not Found",
@@ -76,6 +79,72 @@ describe Kontena::Websocket::Client do
         expect{
           subject.run
         }.to raise_error(WebSocket::Driver::ProtocolError, 'Error during WebSocket handshake: Unexpected response code: 404')
+      end
+    end
+
+    context "with an random SSL cert" do
+      let(:ssl_cn) do
+        'test'
+      end
+      let(:ssl_subject) do
+        OpenSSL::X509::Name.parse "/CN=#{ssl_cn}"
+      end
+      let(:ssl_key) do
+        ssl_key = OpenSSL::PKey::RSA.new(1024)
+      end
+      let(:ssl_cert) do
+        key = ssl_key
+
+        cert = OpenSSL::X509::Certificate.new
+        cert.version = 2
+        cert.serial = 2
+        cert.subject = ssl_subject
+        cert.issuer = cert.subject # self-signe
+        cert.public_key = key.public_key
+        cert.not_before = Time.now - 60.0
+        cert.not_after = cert.not_before + 60.0 # +/- 1 minute validity
+
+        ef = OpenSSL::X509::ExtensionFactory.new
+        ef.subject_certificate = cert
+        ef.issuer_certificate = cert
+
+        cert.add_extension(ef.create_extension("basicConstraints","CA:TRUE",true))
+        cert.add_extension(ef.create_extension("keyUsage","keyCertSign, cRLSign", true))
+        cert.add_extension(ef.create_extension("subjectKeyIdentifier","hash",false))
+        cert.sign(key, OpenSSL::Digest::SHA256.new)
+        cert
+      end
+      let(:ssl_context) do
+        ssl_context = OpenSSL::SSL::SSLContext.new
+        ssl_context.cert = ssl_cert
+        ssl_context.key = ssl_key
+        ssl_context
+      end
+      let(:ssl_server) do
+        OpenSSL::SSL::SSLServer.new(tcp_server, ssl_context)
+      end
+      let(:server_thread) do
+        Thread.new do
+          loop do
+            begin
+              client = ssl_server.accept
+              client.readpartial(1024)
+              client.close
+            rescue => exc
+              logger.warn exc
+            end
+          end
+        end
+      end
+
+      context "with ssl verify" do
+        subject { described_class.new("wss://127.0.0.1:#{port}", ssl_verify: true) }
+
+        it 'raises a SSL error' do
+          expect{
+            subject.run
+          }.to raise_error(OpenSSL::SSL::SSLError, 'SSL_connect returned=1 errno=0 state=error: certificate verify failed')
+        end
       end
     end
   end
