1309a3d326657e14914e5fcef2d41dce03a60938
Tero Marttila <tero.marttila@kontena.io>
2017-07-06 19:27:28 +0300

    common websocket-client spec: HTTP 404 response

--- spec/kontena/websocket/spec.rb
+++ spec/kontena/websocket/spec.rb
@@ -17,21 +17,13 @@ describe Kontena::Websocket::Client do
     end
   end
 
-  context "For a server that immediately closes the connection" do
+  context "For a local server" do
     let(:server) do
       TCPServer.new('127.0.0.1', 0)
     end
     let(:port) do
       af, port, hostname, ip = server.addr
       port
-    end
-    let(:server_thread) do
-      Thread.new do
-        loop do
-          client = server.accept
-          client.close
-        end
-      end
     end
 
     before do
@@ -45,16 +37,46 @@ describe Kontena::Websocket::Client do
 
     subject { described_class.new("ws://127.0.0.1:#{port}") }
 
-    it 'raises a EOF error' do
-      opened = false
-
-      expect{
-        subject.run do
-          opened = true
+    context "that immediately closes the connection" do
+      let(:server_thread) do
+        Thread.new do
+          loop do
+            client = server.accept
+            client.close
+          end
         end
-      }.to raise_error(Kontena::Websocket::EOFError, 'Connection closed with code 1006: EOF')
+      end
 
-      expect(opened).to be false
+      it 'raises a EOF error' do
+        expect{
+          subject.run
+        }.to raise_error(Kontena::Websocket::EOFError, 'Connection closed with code 1006: EOF')
+      end
+    end
+
+    context "that returns a HTTP 404 error" do
+      let(:server_thread) do
+        Thread.new do
+          loop do
+            client = server.accept
+            client.readpartial(1024)
+            client.write([
+              "HTTP/1.1 404 Not Found",
+              "Server: test",
+              "Connection: close",
+              "",
+              "",
+            ].join("\r\n"))
+            #client.close
+          end
+        end
+      end
+
+      it 'raises a protocol error' do
+        expect{
+          subject.run
+        }.to raise_error(WebSocket::Driver::ProtocolError, 'Error during WebSocket handshake: Unexpected response code: 404')
+      end
     end
   end
 end
