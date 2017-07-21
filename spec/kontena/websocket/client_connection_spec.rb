62da5db1751ce0663ca37cab2f954aabbb70f0f9
Tero Marttila <tero.marttila@kontena.io>
2017-07-21 19:33:38 +0300

    fix connection write spec

--- spec/kontena/websocket/client_connection_spec.rb
+++ spec/kontena/websocket/client_connection_spec.rb
@@ -10,42 +10,44 @@ RSpec.describe Kontena::Websocket::Client::Connection do
     expect(subject.url).to eq 'ws://socket.example.com'
   end
 
-  it 'writes to the socket' do
-    expect(socket).to receive(:write_nonblock).with('asdf').and_return(4)
+  describe '#write' do
+    it 'writes to the socket' do
+      expect(socket).to receive(:write_nonblock).with('asdf').and_return(4)
 
-    subject.write('asdf')
-  end
+      subject.write('asdf')
+    end
 
-  it 'writes a large string to the socket in multiple chunks' do
-    expect(socket).to receive(:write_nonblock).with('asdf' * 16).and_return(4 * 8)
-    expect(socket).to receive(:write_nonblock).with('asdf' * 8).and_return(4 * 4)
-    expect(socket).to receive(:write_nonblock).with('asdf' * 4).and_return(4 * 4)
+    it 'writes a large string to the socket in multiple chunks' do
+      expect(socket).to receive(:write_nonblock).with('asdf' * 16).and_return(4 * 8)
+      expect(socket).to receive(:write_nonblock).with('asdf' * 8).and_return(4 * 4)
+      expect(socket).to receive(:write_nonblock).with('asdf' * 4).and_return(4 * 4)
 
-    subject.write('asdf' * 16)
-  end
-
-  it 'waits on write without a timeout' do
-    expect(socket).to receive(:write_nonblock).with('asdf').and_raise(Class.new(Errno::ETIMEDOUT) do
-      include IO::WaitWritable
-    end)
-    expect(subject).to receive(:wait_socket_readable!).with(socket, nil).and_return(socket)
-    expect(socket).to receive(:write_nonblock).with('asdf').and_return(4)
-
-    subject.write('asdf')
-  end
-
-  context 'with a write timeout' do
-    subject { described_class.new(uri, socket, write_timeout: 1.0) }
+      subject.write('asdf' * 16)
+    end
 
     it 'waits on write without a timeout' do
       expect(socket).to receive(:write_nonblock).with('asdf').and_raise(Class.new(Errno::ETIMEDOUT) do
         include IO::WaitWritable
       end)
-      expect(subject).to receive(:wait_socket_writable!).with(socket, 1.0).and_return(nil)
+      expect(subject).to receive(:wait_socket_writable!).with(socket, nil).and_return(socket)
+      expect(socket).to receive(:write_nonblock).with('asdf').and_return(4)
 
-      expect{
-        subject.write('asdf')
-      }.to raise_error(Kontena::Websocket::TimeoutError, 'write timeout after 1.0s')
+      subject.write('asdf')
+    end
+
+    context 'with a write timeout' do
+      subject { described_class.new(uri, socket, write_timeout: 1.0) }
+
+      it 'waits on write without a timeout' do
+        expect(socket).to receive(:write_nonblock).with('asdf').and_raise(Class.new(Errno::ETIMEDOUT) do
+          include IO::WaitWritable
+        end)
+        expect(subject).to receive(:wait_socket_writable!).with(socket, 1.0).and_raise(Kontena::Websocket::TimeoutError, 'write timeout after 1.0s')
+
+        expect{
+          subject.write('asdf')
+        }.to raise_error(Kontena::Websocket::TimeoutError, 'write timeout after 1.0s')
+      end
     end
   end
 end
