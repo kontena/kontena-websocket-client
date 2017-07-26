5ab927473a00e37073624e0f721c4bd85f6a0854
Tero Marttila <tero.marttila@kontena.io>
2017-07-26 14:14:05 +0300

    BENCHMARK=benchmark-em.rb ./benchmark.sh

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -4,6 +4,7 @@ set -uex
 
 DIR=$(dirname $0)
 SERVER_ARGS=${SERVER_ARGS:- -drop}
+BENCHMARK=${BENCHMARK:-benchmark-client.rb}
 
 go get -d github.com/gorilla/websocket
 go build -o $DIR/websocket-echo-server $DIR/websocket-echo-server.go
@@ -11,6 +12,6 @@ go build -o $DIR/websocket-echo-server $DIR/websocket-echo-server.go
 killall websocket-echo-server || true
 $DIR/websocket-echo-server -quiet $SERVER_ARGS &
 
-$DIR/benchmark-client.rb
+$DIR/${BENCHMARK}
 
 trap "kill 0" INT EXIT
