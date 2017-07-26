7c8664bbcfc42a336200e09c71306259f3b7dcd6
Tero Marttila <tero.marttila@kontena.io>
2017-07-26 11:55:49 +0300

    benchmark: enable drop mode for server

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -8,7 +8,7 @@ go get -d github.com/gorilla/websocket
 go build -o $DIR/websocket-echo-server $DIR/websocket-echo-server.go
 
 killall websocket-echo-server || true
-$DIR/websocket-echo-server -quiet &
+$DIR/websocket-echo-server -quiet -drop &
 $DIR/websocket-benchmark.rb &
 
 trap "kill 0" INT EXIT
