801dadb9da566f83f133841d0d784a583995c4e0
Tero Marttila <tero.marttila@kontena.io>
2017-07-26 13:46:23 +0300

    refactor benchmark

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -3,13 +3,14 @@
 set -uex
 
 DIR=$(dirname $0)
+SERVER_ARGS=${SERVER_ARGS:- -drop}
 
 go get -d github.com/gorilla/websocket
 go build -o $DIR/websocket-echo-server $DIR/websocket-echo-server.go
 
 killall websocket-echo-server || true
-$DIR/websocket-echo-server -quiet -drop &
+$DIR/websocket-echo-server -quiet $SERVER_ARGS &
 
-$DIR/websocket-benchmark.rb
+$DIR/benchmark-client.rb
 
 trap "kill 0" INT EXIT
