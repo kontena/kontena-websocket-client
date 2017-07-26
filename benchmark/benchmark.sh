bfb7751693315e603366aa9a9d1623ff671d7ea6
Tero Marttila <tero.marttila@kontena.io>
2017-07-26 12:09:34 +0300

    fix benchmark.sh wait

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -9,7 +9,7 @@ go build -o $DIR/websocket-echo-server $DIR/websocket-echo-server.go
 
 killall websocket-echo-server || true
 $DIR/websocket-echo-server -quiet -drop &
-$DIR/websocket-benchmark.rb &
+
+$DIR/websocket-benchmark.rb
 
 trap "kill 0" INT EXIT
-wait
